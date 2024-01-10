599f5b3267b552afd1743ac8df5ad93caad7d253
SupanutT <supanut.tangsin@gmail.com>
2024-01-11 03:03:29 +0700

    Ch.12 : Styles Clean Up

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,24 +1,20 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v11',
     center: [101, 13.7],
     zoom: 4
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 map.on('load', () => {
-    // Add a new source from our GeoJSON data and
-    // set the 'cluster' option to true. GL-JS will
-    // add the point_count property to your source data.
     map.addSource('campgrounds', {
         type: 'geojson',
-        // Point to GeoJSON data. This example visualizes all M1.0+ campgrounds
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
         data: campgrounds,
         cluster: true,
-        clusterMaxZoom: 14, // Max zoom to cluster points on
-        clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+        clusterMaxZoom: 14,
+        clusterRadius: 50
     });
 
     map.addLayer({
@@ -27,11 +23,6 @@ map.on('load', () => {
         source: 'campgrounds',
         filter: ['has', 'point_count'],
         paint: {
-            // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
-            // with three steps to implement three types of circles:
-            //   * Blue, 20px circles when point count is less than 100
-            //   * Yellow, 30px circles when point count is between 100 and 750
-            //   * Pink, 40px circles when point count is greater than or equal to 750
             'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
             'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
         }
@@ -62,7 +53,6 @@ map.on('load', () => {
         }
     });
 
-    // inspect a cluster on click
     map.on('click', 'clusters', (e) => {
         const features = map.queryRenderedFeatures(e.point, {
             layers: ['clusters']
@@ -78,10 +68,6 @@ map.on('load', () => {
         });
     });
 
-    // When a click event occurs on a feature in
-    // the unclustered-point layer, open a popup at
-    // the location of the feature, with
-    // description HTML from its properties.
     map.on('click', 'unclustered-point', (e) => {
         const coordinates = e.features[0].geometry.coordinates.slice();
         const { popUpMarkUp } = e.features[0].properties;
