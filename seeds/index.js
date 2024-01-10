fbcbda47be84b6877abee1f4d3a6db10269c43a4
SupanutT <supanut.tangsin@gmail.com>
2024-01-11 02:05:14 +0700

    Ch.11 : Fancy Cluster Map

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,7 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapboxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -25,12 +18,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random923 = Math.floor(Math.random() * 923);
         const price = Math.floor(Math.random() * 1500) + 500;
         const camp = new Campground({
             author: '659da476dfee85db545edfcd',
             location: `${cities[random923].AMPHOE_E}, ${cities[random923].CHANGWAT_E}`,
+            geometry: {
+                type: 'Point',
+                coordinates: [cities[random923].LONG, cities[random923].LAT]
+            },
             title: `${sample(descriptors)} ${sample(places)}`,
             description:
                 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, quae. Deleniti animi, amet ducimus fuga ab facilis voluptate nisi pariatur fugit veritatis ipsa totam sapiente architecto laudantium est incidunt dolorum.',
@@ -54,13 +51,6 @@ const seedDB = async () => {
                 }
             ]
         });
-        const geoData = await geocoder
-            .forwardGeocode({
-                query: camp.location,
-                limit: 1
-            })
-            .send();
-        camp.geometry = geoData.body.features[0].geometry;
         await camp.save();
     }
     console.log('SEEDING DONE');
