5cc4837189a72b0d0707285b9be9876dfc2aced6
SupanutT <supanut.tangsin@gmail.com>
2024-01-10 18:13:27 +0700

    Ch.8 : Controllers & Star Ratings

--- middleware.js
+++ middleware.js
@@ -41,8 +41,9 @@ module.exports.validateReview = (req, res, next) => {
 
 module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
+    console.log(req.user._id);
     const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user_id)) {
+    if (!campground.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     } else {
@@ -53,7 +54,7 @@ module.exports.isAuthor = async (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user_id)) {
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     } else {
